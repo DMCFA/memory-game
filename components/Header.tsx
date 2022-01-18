a2e6a859698d43aab08c71b888b1751e2297b28f
Duarte Almeida <duartemalmeida@gmail.com>
2022-01-18 19:34:14 +0000

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+import React, { ReactElement } from 'react';
+import styled from 'styled-components';
+
+const Header: React.FC = (): ReactElement => {
+  return <h2>Header</h2>;
+};
+
+export default Header;
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+import Header from '../components/Header';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Hello</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
