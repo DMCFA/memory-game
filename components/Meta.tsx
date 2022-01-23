35882d59e0d34c93aff164358772295751a39476
Duarte Almeida <duartemalmeida@gmail.com>
2022-01-23 19:28:11 +0000

    feat! ]add meta component

---
+++ components/Meta.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Meta = () => {
+  return (
+    <Head>
+      <title>Memory Game</title>
+      <meta name='description' content='Browser Memory Game' />
+      <meta name='keywords' content='game memory fun browser online' />
+      <link rel='icon' href='/--logo.png' />
+    </Head>
+  );
+};
+
+export default Meta;
