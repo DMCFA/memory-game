545dc34629cf971ba0366306c7ef8cd7e3269742
Duarte Almeida <duartemalmeida@gmail.com>
2022-01-27 20:17:45 +0000

    feat: add home and rules links

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,16 @@
+import Link from 'next/link';
 import { StyledNav } from '../styles/Nav.styled';
 
 const Nav = () => {
   return (
     <StyledNav>
       <h1>memory game</h1>
+      <Link href='/'>
+        <a>home</a>
+      </Link>
+      <Link href='/rules'>
+        <a>rules</a>
+      </Link>
     </StyledNav>
   );
 };
