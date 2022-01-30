f7bd85d5a312142acaa9cc8b972b9362a201e830
Duarte Almeida <duartemalmeida@gmail.com>
2022-01-30 15:35:58 +0000

    feat: add layout style

---
+++ styles/Layout.styled.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledLayout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`;
+
+export default StyledLayout;
