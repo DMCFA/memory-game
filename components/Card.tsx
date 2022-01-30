511b7c23de013982962872cd2cca2d903f81e93d
Duarte Almeida <duartemalmeida@gmail.com>
2022-01-30 15:32:53 +0000

    feat: create card component

---
+++ components/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StyledCard from '../styles/Card.styled';
+
+const Card = () => {
+  return (
+    <>
+      <StyledCard />
+    </>
+  );
+};
+
+export default Card;
