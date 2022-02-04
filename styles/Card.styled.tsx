f3459ea72f8c9d935e18bfa32e5b46b84cf4634c
Duarte Almeida <duartemalmeida@gmail.com>
2022-02-04 23:01:14 +0000

    feat: add styles

---
+++ styles/Card.styled.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StyledCard = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+`;
+
+export default StyledCard;
