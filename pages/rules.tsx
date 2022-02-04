19b754607459989695afe159886c710a856f5fc8
Duarte Almeida <duartemalmeida@gmail.com>
2022-02-04 23:00:14 +0000

    feat: add text content

--- pages/rules.tsx
+++ pages/rules.tsx
@@ -10,9 +10,22 @@ const Rules: React.FC = (): ReactElement => {
   return (
     <StyledRules>
       <StyledH2>Rules</StyledH2>
-      <StyledH3>Game Objective</StyledH3>
+      <StyledH3>game objective</StyledH3>
       <StyledP>
-        The object of the game is to collect the most matching pairs.
+        The object of the game is to collect the most matching pairs.<br></br>
+        On the game board, there are always two identical images.<br></br>
+        Start the game by flipping a card. Then try to find another card that
+        has the same image as the first. <br></br>If you can't find a pair, the
+        flipped cards will be flipped back with the face down.<br></br> Try to
+        remember these images as it becomes easier to find pairs the longer you
+        play.<br></br>
+        When you find a pair they are removed from the board and when you find
+        all the pairs you have completed the game.
+      </StyledP>
+      <StyledH3>score</StyledH3>
+      <StyledP>
+        Your number of moves is recorded on the screen, the less moves you make
+        to finish the game the better you score.
       </StyledP>
     </StyledRules>
   );
