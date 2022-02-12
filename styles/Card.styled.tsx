6385fc0bd6b233a8a2ff130ed2b91a74d6bcb382
Duarte Almeida <duartemalmeida@gmail.com>
2022-02-12 20:33:39 +0000

    feat: add styles to button and main container

--- styles/Card.styled.tsx
+++ styles/Card.styled.tsx
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
 
-const StyledCard = styled.div`
+export const StyledContainer = styled.div`
+  padding: 5rem;
+`;
+
+export const StyledCard = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   grid-template-rows: repeat(4, 1fr);
 `;
 
-export default StyledCard;
+export const StyledNewGameButton = styled.button`
+  cursor: pointer;
+  margin: 0 auto;
+  padding: 5px;
+`;
