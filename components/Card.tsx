3838cf7e92c928e1ee9b19f54522c976b9e1bcf7
Duarte Almeida <duartemalmeida@gmail.com>
2022-02-12 20:33:13 +0000

    feat: create card structure

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import StyledCard from '../styles/Card.styled';
+import React, { useState } from 'react';
+import { Images } from '../pages/index';
+import {
+  StyledCard,
+  StyledContainer,
+  StyledNewGameButton,
+} from '../styles/Card.styled';
 
-const Card = () => {
+const Card: React.FC<Images> = (props): JSX.Element => {
   return (
     <>
-      <StyledCard />
+      <StyledContainer>
+        <StyledNewGameButton>new game</StyledNewGameButton>
+        <StyledCard key={props.id}>
+          <img src={props.src} alt='front image' />
+        </StyledCard>
+        ;
+      </StyledContainer>
     </>
   );
 };
