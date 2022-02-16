e2b1d9934f41cf1b8b0feb3a0fc7a6d735b8bac7
Duarte Almeida <duartemalmeida@gmail.com>
2022-02-16 21:39:11 +0000

    feat! restructure index file
    - add properties to Images interface
    
    -change the shuffle cards function to return the created card object
    
    -change initializing shuffleCards function in the initial cards state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,27 @@
 import type { NextPage } from 'next';
 import { useEffect, useState } from 'react';
+import Card from '../components/Card';
 import {
   StyledContainer,
   StyledNewGameButton,
   StyledCard,
 } from '../styles/Card.styled';
 
+//types
 export interface Images {
+  id: number;
+  frontCard: string;
+  backCard: string;
+  flipped: boolean;
+  clicked: boolean;
+}
+
+export interface ImageCard {
   id: number;
   src: string;
 }
 
-const images: Images[] = [
+const images: ImageCard[] = [
   { id: 0, src: '../public/images/alpine.png' },
   { id: 1, src: '../public/images/apple.png' },
   { id: 2, src: '../public/images/credit-card.png' },
@@ -24,47 +34,44 @@ const images: Images[] = [
   { id: 9, src: '../public/images/wristwatch.png' },
 ];
 
+//helper function to shuffle cards
+const swapCards = (arr: Images[], i: number, j: number) => {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+};
+
+//shuffle cards function --> duplicates the array, shuffles and sets the new array and turns the move count to zero
+const shuffleCards = (): Images[] => {
+  const newArr: any[] = [...images, ...images];
+  for (let i = newArr.length; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * i);
+    const currentIndex = i - 1;
+    swapCards(newArr, randomIndex, currentIndex);
+  }
+  return newArr.map((card) => ({
+    id: card.id,
+    frontCard: card.src,
+    backCard: 'images/cover.png',
+    flipped: false,
+    clicked: false,
+  }));
+};
+
 const Home: NextPage = (props) => {
-  const [cards, setCards] = useState({});
-  const [moves, setMoves] = useState(0);
-
-  //helper function to shuffle cards
-  const swapCards = (arr: Images[], i: number, j: number) => {
-    const temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-  };
-
-  //shuffle cards function --> duplicates the array, shuffles and sets the new array and turns the move count to zero
-  const shuffleCards = (arr: Images[]): void => {
-    const newArr: Images[] = [...arr, ...arr];
-    for (let i = newArr.length; i > 0; i--) {
-      const randomIndex = Math.floor(Math.random() * i);
-      const currentIndex = i - 1;
-      swapCards(newArr, randomIndex, currentIndex);
-    }
-    setCards(newArr);
-    setMoves(0);
-  };
-
-  useEffect(() => {
-    shuffleCards(images);
-  }, []);
+  const [cards, setCards] = useState<Images[]>(shuffleCards());
+  const [moves, setMoves] = useState<Number>(0);
 
   return (
     <StyledContainer>
       <h1>Memory Game</h1>
-      <StyledNewGameButton onClick={() => shuffleCards(images)}>
+      <StyledNewGameButton onClick={() => shuffleCards()}>
         New Game
       </StyledNewGameButton>
       <StyledCard>
-        {cards.map((card) => {
-          <div key={card.id}>
-            <div>
-              <img src={card.src} alt='card image' />
-            </div>
-          </div>;
-        })}
+        {cards.map((card) => (
+          <Card />
+        ))}
       </StyledCard>
     </StyledContainer>
   );
