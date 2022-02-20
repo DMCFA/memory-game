d6c43bb4ed70e171cc567d2796f4b6b677dbdc24
Duarte Almeida <duartemalmeida@gmail.com>
2022-02-20 15:59:25 +0000

    fix: import fonts

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -6,6 +6,12 @@ const Meta = () => {
       <title>Memory Game</title>
       <meta name='description' content='Browser Memory Game' />
       <meta name='keywords' content='game memory fun browser online' />
+      <link rel='preconnect' href='https://fonts.googleapis.com' />
+      <link rel='preconnect' href='https://fonts.gstatic.com' />
+      <link
+        href='https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;700&display=swap'
+        rel='stylesheet'
+      ></link>
       <link rel='icon' href='/--logo.png' />
     </Head>
   );
