9ce23fd4bb73d999cf394c4d5bd057e4877a1d61
Duarte Almeida <duartemalmeida@gmail.com>
2022-02-24 17:53:59 +0000

    fix: stylesheets statments moved to document

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -4,15 +4,6 @@ const Meta = () => {
   return (
     <Head>
       <title>Memory Game</title>
-      <meta name='description' content='Browser Memory Game' />
-      <meta name='keywords' content='game memory fun browser online' />
-      <link rel='preconnect' href='https://fonts.googleapis.com' />
-      <link rel='preconnect' href='https://fonts.gstatic.com' />
-      <link
-        href='https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;700&display=swap'
-        rel='stylesheet'
-      ></link>
-      <link rel='icon' href='/--logo.png' />
     </Head>
   );
 };
