63a4a3fa16150479cea2736b320b5004a0ca1ff8
Duarte Almeida <duartemalmeida@gmail.com>
2022-02-25 10:32:49 +0000

    feat: add media queries

--- styles/Nav.styled.tsx
+++ styles/Nav.styled.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const StyledNav = styled.nav`
   display: flex;
+  justify-content: space-around;
   width: 100%;
   height: fit-content;
   background-color: #325288;
@@ -12,12 +13,25 @@ export const StyledNav = styled.nav`
     font-variant: small-caps;
     padding-inline-start: 4rem;
     line-height: 110%;
+
+    @media screen and (max-width: 1451px) {
+      font-size: 2rem;
+      line-height: 180%;
+    }
+    @media screen and (max-width: 1150px) {
+      font-size: 1.5rem;
+      line-height: 225%;
+    }
   }
 `;
 
 export const StyledLinks = styled.div`
   display: flex;
   margin-left: 60%;
+
+  @media screen and (max-width: 1000px) {
+    margin: 0;
+  }
 
   a {
     text-decoration: none;
@@ -33,6 +47,16 @@ export const StyledLinks = styled.div`
     color: #faeee7;
     border: 2px solid white;
     transition: all 0.5s;
+
+    @media screen and (max-width: 1451px) {
+      font-size: 1.1rem;
+    }
+    @media screen and (max-width: 1150px) {
+      font-size: 1rem;
+    }
+    @media screen and (max-width: 1000px) {
+      font-size: 1rem;
+    }
 
     &:hover {
       transform: scale(1.2);
