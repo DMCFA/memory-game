868accd2dfa640565d45d1bde13e44fbf5aa2568
Duarte Almeida <duartemalmeida@gmail.com>
2022-02-25 10:33:10 +0000

    feat: add styles for main grid

--- styles/Card.styled.tsx
+++ styles/Card.styled.tsx
@@ -1,17 +1,101 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const StyledContainer = styled.div`
-  padding: 5rem;
+interface Props {
+  flipped: boolean;
+}
+
+const images = css`
+  width: auto;
+  height: auto;
+  cursor: pointer;
+  transition: all 0.8s;
+  backface-visibility: hidden;
 `;
 
-export const StyledCard = styled.div`
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  grid-template-rows: repeat(4, 1fr);
+export const StyledContainer = styled.div`
+  padding: 3rem;
+
+  @media screen and (max-width: 1600px) {
+    padding: 0.7rem;
+  }
+`;
+
+export const StyledGameHeader = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+export const StyledResults = styled.h4`
+  color: #325288;
+  font-size: 2.4rem;
+  font-variant: small-caps;
 `;
 
 export const StyledNewGameButton = styled.button`
+  /* display: block !important;
+  margin: 0 auto; */
+  box-shadow: inset 0px 1px 0px 0px #97c4fe;
+  background: linear-gradient(to bottom, #325288 5%, #205dbe 100%);
+  background-color: #325288;
+  border-radius: 6px;
+  border: 1px solid #325288;
+  display: inline-block;
   cursor: pointer;
-  margin: 0 auto;
-  padding: 5px;
+  color: #ffffff;
+  font-size: 1.3rem;
+  font-weight: bold;
+  padding: 0.8rem 1.2rem;
+  text-transform: uppercase;
+  text-shadow: 0px 1px 0px #325288;
+
+  @media screen and (max-width: 1451px) {
+    padding: 0.7rem;
+  }
+
+  &:hover {
+    background: linear-gradient(to bottom, #1e62d0 5%, #3d94f6 100%);
+    background-color: #1e62d0;
+  }
+
+  &:active {
+    position: relative;
+    top: 1px;
+  }
+`;
+
+export const StyledGrid = styled.div`
+  margin-top: 5rem;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  row-gap: 1.5rem;
+  justify-items: center;
+
+  @media screen and (max-width: 1600px) {
+    margin-top: 1.5rem;
+    row-gap: 0.5rem;
+  }
+`;
+
+export const StyledCard = styled.div`
+  position: relative;
+
+  .front .flipped {
+    z-index: 1;
+    transform: rotateY(180deg);
+  }
+`;
+
+export const StyledFrontCard = styled.img<Props>`
+  ${images}
+  transform: ${(props) => (props.flipped ? 'rotate(0deg)' : 'rotateY(180deg)')};
+  z-index: ${(props) => (props.flipped ? '2' : '1')};
+`;
+
+export const StyledBackCard = styled.img<Props>`
+  ${images}
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: ${(props) =>
+    props.flipped ? 'rotateY(180deg)' : 'rotate(360deg)'};
+  z-index: ${(props) => (props.flipped ? '1' : '2')};
 `;
